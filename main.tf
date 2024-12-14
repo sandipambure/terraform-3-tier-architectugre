4e886f0249442507cf7d10eda5c64a1f973390e5
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-12-14 16:27:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -358,10 +358,10 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "rahamtestbycketterra7788abcdefxxc54hj6jfegfefgrg"
+  bucket = "rahamtestbycketterra7788abcdededded0or40ffxxc"
 
   tags = {
-    Name        = "rahamtestbycketterra7788abcdefxxc"
+    Name        = "rahamtestbycketterra7788abcdededded0or40ffxxc"
     Environment = "Dev"
   }
 }
